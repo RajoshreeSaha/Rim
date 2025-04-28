66a9ec8f63333ed004c8767c23be3de5ca9a932d
RajoshreeSaha <rajoshreesaha171@gmail.com>
2025-04-28 17:48:25 +0000

    # initial questions

---
+++ sayantan/BasicCalculator.js
@@ -0,0 +1,3 @@
+//A simple calculator that can add two numbers.
+let a = 2, b = 3
+console.log(a+b)
---
+++ sayantan/SimpleIfElse.js
@@ -0,0 +1,9 @@
+//use if else in simple manner
+let a = 0
+if (a>0) {
+    console.log("positive number");
+} else if(a<0){
+    console.log("negative number");
+} else {
+    console.log("zero");
+}
